06d60ccdec0b7908a109cf822879b3d40150d1ff
AyushBatra15 <batraayush@icloud.com>
2023-04-14 14:47:52 -0400

    updated prep file

--- preparation.R
+++ preparation.R
@@ -50,9 +50,22 @@ for (t in team_ids) {
   print(paste("Loaded ",iter,"/30 Teams", sep = ""))
 }
 
+matchups <- all %>%
+  mutate(across(c(PARTIAL_POSS : MATCHUP_TIME_SEC), as.numeric))
 
+matchups <- matchups %>%
+  left_join(pts, by = c("OFF_PLAYER_ID" = "PLAYER_ID")) %>%
+  left_join(mins, by = c("OFF_PLAYER_ID" = "PLAYER_ID"))
 
+matchups <- matchups %>%
+  mutate(Season = SEASON,
+         numSeason = as.numeric(str_sub(Season, 1, 4)) + 1) %>%
+  select(Season, numSeason, 
+         OFF_PLAYER_ID, OFF_PLAYER_NAME, DEF_PLAYER_ID, DEF_PLAYER_NAME,
+         PARTIAL_POSS, PLAYER_PTS, MATCHUP_FGA, MATCHUP_FTA, MATCHUP_TOV,
+         OFF_PLAYER_SZN_PTS = PTS, OFF_PLAYER_SZN_MIN = MIN)
 
+write_csv(matchups, file = "data/matchups2023_rs.csv")
 
 
 
