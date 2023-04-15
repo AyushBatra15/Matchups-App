683d36e7e948ff1610091b52c5149831b3279835
AyushBatra15 <batraayush@icloud.com>
2023-04-15 13:17:26 -0400

    added player lists

--- shiny.R
+++ shiny.R
@@ -5,3 +5,24 @@ library(gt)
 library(webshot)
 library(ggimage)
 library(scales)
+
+matchups <- read_csv(url("https://raw.githubusercontent.com/AyushBatra15/Matchups-App/main/data/all_matchups.csv"))
+defstats <- read_csv(url("https://raw.githubusercontent.com/AyushBatra15/Matchups-App/main/data/defstats.csv"))
+headshots <- read_csv(url("https://raw.githubusercontent.com/AyushBatra15/Matchups-App/main/data/player_headshots.csv"))
+team_info <- read_csv(url("https://raw.githubusercontent.com/AyushBatra15/Matchups-App/main/data/team_info.csv"))
+
+player_mins <- defstats %>%
+  select(PLAYER_ID, numSeason, Season, MIN)
+
+player_options <- defstats %>%
+  filter(MIN >= 500) %>%
+  distinct(PLAYER_NAME) %>%
+  pull(PLAYER_NAME)
+
+season_options <- defstats %>%
+  distinct(Season) %>%
+  pull(Season)
+
+
+
+
